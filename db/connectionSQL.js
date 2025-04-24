4ce141a1e7fd1916bdc2418da61a62b5d5c894f3
martinxanton <david.morillo1@unmsm.edu.pe>
2025-04-24 09:37:31 -0500

    chore: Revert to hardcoded PostgreSQL connection settings and remove dotenv configuration

--- db/connectionSQL.js
+++ db/connectionSQL.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
 
 // Crear la instancia de Sequelize para PostgreSQL
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize('postgres', 'postgres', 'Martin1614*', {
+  host: 'db.pbojqyicmjomazinfnsb.supabase.co',
+  port: 5432,
   dialect: 'postgres',
   logging: console.log,
   dialectOptions: {
