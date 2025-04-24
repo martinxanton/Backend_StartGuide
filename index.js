425c0f7433de36e5450d0a15e1caae72c722f27c
martinxanton <david.morillo1@unmsm.edu.pe>
2025-04-23 22:55:44 -0500

    chore: Update server port configuration to use environment variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connectMongoDB = require('./db/connectionMongoDB');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Middleware
 app.use(cors());
