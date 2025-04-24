b5118be2aa715ab61309484dbfc8e96ee7707bab
martinxanton <david.morillo1@unmsm.edu.pe>
2025-04-23 23:22:08 -0500

    chore: Add SSL options to Sequelize configuration for PostgreSQL

--- db/connectionSQL.js
+++ db/connectionSQL.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   port: process.env.DB_PORT,
   dialect: 'postgres',
   logging: console.log,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 // Exportar la instancia de sequelize
